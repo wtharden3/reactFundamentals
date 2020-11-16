b1a7ca52f99ad9fc3869c753d03ea20ffbf0d9fb
Whitney Harden <wtharden320@gmail.com>
2020-11-16 09:07:39 -0500

    Effects.js

--- src/components/concepts/Effects.js
+++ src/components/concepts/Effects.js
@@ -1,58 +1,70 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Effects = () => {
-  return(
+  return (
     <div className="main">
       <div className="mainDiv text-left">
         <h2>Below are some important points regarding effects:</h2>
         <ul>
-          <li>They are triggered with state and prop updates, as well as after initial render to the DOM.</li>
-          <li>Effects are used to trigger certain actions based upon changes tot he componenet.</li>
-          <li>Effects can return cleanup functions, which can clean up timers using memory, listeners to outside the APIs, and other functions which may use system resources.</li>
+          <li>
+            They are triggered with state and prop updates, as well as after
+            initial render to the DOM.
+          </li>
+          <li>
+            Effects are used to trigger certain actions based upon changes tot
+            he componenet.
+          </li>
+          <li>
+            Effects can return cleanup functions, which can clean up timers
+            using memory, listeners to outside the APIs, and other functions
+            which may use system resources.
+          </li>
         </ul>
         <SampleEffect />
         <br />
         <SampleEffect />
       </div>
     </div>
-  )
-}
+  );
+};
 
 const SampleEffect = () => {
   const [timerRunning, setTimerRunning] = useState(false);
-  
-  useEffect( () => {
+
+  useEffect(() => {
     let timer;
-    if (timerRunning){
-      timer = window.setTimeout( ()=>{
-        console.log('the timer expired', Date.now()/1000);
+    if (timerRunning) {
+      timer = window.setTimeout(() => {
+        console.log('the timer expired', Date.now() / 1000);
         setTimerRunning(false);
-      }, 2000)
+      }, 2000);
     }
     return () => {
       window.clearTimeout(timer);
-      console.log('the timer was cleaned up', Date.now()/1000)
+      console.log('the timer was cleaned up', Date.now() / 1000);
+    };
+  });
+
+  useEffect(() => {
+    if(timerRunning){
+      console.log('This paint needs some happy trees')
     }
-  })
+  }, [timerRunning]);
 
   let buttonHandler = () => {
-    if (!timerRunning){
+    if (!timerRunning) {
       setTimerRunning(true);
     }
-  }
+  };
 
-  let headerHover = () => {
-    if(!timerRunning){
-      setTimerRunning(true)
-    }
-  }
-
-  return(
-    <div style={{border: '1px dashed black'}}>
-      <h2 onMouseOver={headerHover}>This componenet demoes an effect</h2>
-      <button onClick={buttonHandler}>Click me to start an effect in the console.</button>
+  return (
+    <div style={{ border: '1px dashed black' }}>
+      <h2>This componenet demoes an effect</h2>
+      <button onClick={buttonHandler}>
+        Click me to start an effect in the console.
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Effects;
