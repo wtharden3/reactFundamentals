36827c71388177c036c554e2bc5f76e03dc9d81e
Whitney Harden <wtharden320@gmail.com>
2020-11-21 18:21:43 -0500

    ClockApp completed

--- src/components/apps/timer-apps/TimerApp.js
+++ src/components/apps/timer-apps/TimerApp.js
@@ -1,22 +1,26 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 const TimerApp = () => {
   const [secsElapsed, setSecsElapsed] = useState(0);
+
   const tick = () => {
     setSecsElapsed(secsElapsed + 1);
-  }
+  };
 
-  useEffect( () => {
-    let interval = setInterval(() => tick(), 1000);
-    return () => clearInterval(interval);
-  })
+  useEffect(() => {
+    let secInterval = setInterval(() => tick(), 1000);
+    
+    return () => {
+      clearInterval(secInterval);
+    };
+  });
 
-  return(
+  return (
     <div>
       <h1 className="section-title">React Timer</h1>
       <div>Seconds Elapsed: {secsElapsed}</div>
     </div>
-  )
-}
+  );
+};
 
 export default TimerApp;
